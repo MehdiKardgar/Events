642c02db987ba24504cd36164afb01400641bea7
Mehdi Kardgar <76148286+MehdiKardgar@users.noreply.github.com>
2024-11-03 23:18:05 +0330

    Merging head Content

--- pages/_app.js
+++ pages/_app.js
@@ -9,9 +9,11 @@ import "@/styles/globals.css";
 export default function App({ Component, pageProps }) {
   return (
     <Layout>
-      {/* <Head>
+      <Head>
+        <title>Next Events</title>
+        <meta name="description" content="NextJS Events" />
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-      </Head> */}
+      </Head>
       <Component {...pageProps} />
     </Layout>
   );
