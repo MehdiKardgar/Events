7f90b6ab7d94b226c097fa08db46b946cf926b19
Mehdi Kardgar <76148286+MehdiKardgar@users.noreply.github.com>
2024-11-29 23:47:10 +0330

    new

--- pages/_app.js
+++ pages/_app.js
@@ -1,20 +1,20 @@
-// _app.js
-// my route app component
+// // _app.js
+// // my route app component
 
-import Head from "next/head";
+// import Head from "next/head";
 
-import Layout from "@/components/layout/layout";
-import "@/styles/globals.css";
+// import Layout from "@/components/layout/layout";
+// import "@/styles/globals.css";
 
-export default function App({ Component, pageProps }) {
-  return (
-    <Layout>
-      <Head>
-        <title>Next Events</title>
-        <meta name="description" content="NextJS Events" />
-        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-      </Head>
-      <Component {...pageProps} />
-    </Layout>
-  );
-}
+// export default function App({ Component, pageProps }) {
+//   return (
+//     <Layout>
+//       <Head>
+//         <title>Next Events</title>
+//         <meta name="description" content="NextJS Events" />
+//         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+//       </Head>
+//       <Component {...pageProps} />
+//     </Layout>
+//   );
+// }
