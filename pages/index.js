3cf875e78d37756c4e3ffc92039895cd831095a6
Mehdi Kardgar <76148286+MehdiKardgar@users.noreply.github.com>
2025-01-01 22:55:27 +0330

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,33 +1,33 @@
-// //  This file is our route page, our home page,
+//  This file is our route page, our home page,
 
-// import Head from "next/head";
+import Head from "next/head";
 
-// import EventList from "@/components/events/event-list";
-// import { getFeaturedEvents } from "@/helpers/api-util";
+import EventList from "@/components/events/event-list";
+import { getFeaturedEvents } from "@/helpers/api-util";
 
-// export default function HomePage(props) {
-//   return (
-//     <div>
-//       <Head>
-//         <title>NextJS Events</title>
-//         <meta
-//           name="description"
-//           content="Find a lot of great events that allow you to evolve..."
-//         />
-//       </Head>
+export default function HomePage(props) {
+  return (
+    <div>
+      <Head>
+        <title>NextJS Events</title>
+        <meta
+          name="description"
+          content="Find a lot of great events that allow you to evolve..."
+        />
+      </Head>
 
-//       <EventList items={props.events} />
-//     </div>
-//   );
-// }
+      <EventList items={props.events} />
+    </div>
+  );
+}
 
-// export async function getStaticProps() {
-//   const featuredEvents = await getFeaturedEvents();
+export async function getStaticProps() {
+  const featuredEvents = await getFeaturedEvents();
 
-//   return {
-//     props: {
-//       events: featuredEvents,
-//     },
-//     revalidate: 1800,
-//   };
-// }
+  return {
+    props: {
+      events: featuredEvents,
+    },
+    revalidate: 1800,
+  };
+}
